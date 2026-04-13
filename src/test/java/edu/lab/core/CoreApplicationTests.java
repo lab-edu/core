d07be82c4a14933c0b840638049c07d2a885f96f
xv1rcn <xavierwah@outlook.com>
2026-04-14 04:34:17 +0800

    chore: init spring boot core

---
+++ src/test/java/edu/lab/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

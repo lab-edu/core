8e5ae7e7915385a891ca7530dc8841741645c199
xv1rcn <xavierwah@outlook.com>
2026-04-22 00:43:13 +0800

    fix(openapi): remove hardcoded localhost server, avoid swagger doc/host mismatch in deployment

--- src/main/java/edu/lab/core/OpenApiConfig.java
+++ src/main/java/edu/lab/core/OpenApiConfig.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -20,9 +19,6 @@ import org.springframework.context.annotation.Configuration;
         contact = @Contact(name = "lab-edu team"),
         license = @License(name = "Internal")
     ),
-    servers = {
-        @Server(url = "http://localhost:8080", description = "Local")
-    },
     security = {
         @SecurityRequirement(name = "bearerAuth")
     }
